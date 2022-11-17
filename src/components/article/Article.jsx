73dc71a06ff13a8f186fcab07707e12640d93818
SuraZz007 <surajdev9886@gmail.com>
2022-11-17 17:21:53 +0530

    Add blog post

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./article.css";
 
-const Article = () => {
-  return <div>Article</div>;
+const Article = ({ imageUrl, date, title }) => {
+  return (
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_acticle-image">
+        <img src={imageUrl} alt="blog img" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
 };
 
 export default Article;
